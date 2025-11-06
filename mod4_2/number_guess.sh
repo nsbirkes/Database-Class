ae9d715bb34d0f9ea047df14aaadd5d5c1580d03
nbirkes28 <nsbirkes@gmail.com>
2025-11-05 21:42:51 -0600

    Finished module 4 - number guess

---
+++ mod4_2/number_guess.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo "Enter your username:"
+read USERNAME
+
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_ID ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+echo "Guess the secret number between 1 and 1000:"
+
+NUMBER_OF_GUESSES=0
+
+while true
+do
+  read GUESS
+
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((NUMBER_OF_GUESSES++))
+
+  if (( GUESS == SECRET_NUMBER ))
+  then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    INSERT_GAME=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $NUMBER_OF_GUESSES)")
+    break
+  elif (( GUESS > SECRET_NUMBER ))
+  then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
+
+# commit 2
+# commit 3
+# commit 4
+# commit 5
